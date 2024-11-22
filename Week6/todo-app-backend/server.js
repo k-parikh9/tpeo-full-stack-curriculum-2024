d0c475de0b35b8154e7accb6a2ac11556a027e64
Krish Parikh <kparikh0509@gmail.com>
2024-11-21 19:06:43 -0600

    week 6 partial

--- Week6/todo-app-backend/server.js
+++ Week6/todo-app-backend/server.js
@@ -44,17 +44,47 @@ app.get("/tasks", async (req, res) => {
 // GET: Endpoint to retrieve all tasks for a user
 app.get("/tasks/:user", async (req, res) => {
   try {
-
+    const snapshot = await db.collection("tasks").where("user", "==", req.params.user).get();
+    let tasks = [];
+    snapshot.forEach((doc) => {
+      tasks.push({
+        id: doc.id,
+        ...doc.data(),
+      });
+    });
+    res.status(200).send(tasks);
   } catch (error) {
     res.status(500).send(error.message);
   }
 })
 
 // POST: Endpoint to add a new task
-// ...
+app.post("/tasks", async (req, res) => {
+  try {
+    // Creating a new document in the "tasks" collection with the data received in the request body
+    const docRef = await db.collection("tasks").add(req.body);
+    
+    // Sending a successful response with the ID of the newly created document
+    res.status(201).send(docRef.id);
+  } catch (error) {
+    // Sending an error response in case of an exception
+    res.status(500).send(error.message);
+  }
+});
 
 // DELETE: Endpoint to remove a task
-// ...
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    // Deleting the document with the specified ID from the "tasks" collection
+    await db.collection("tasks").doc(req.params.id).delete();
+    
+    // Sending a successful response
+    res.status(204).send();
+  } catch (error) {
+    // Sending an error response in case of an exception
+    res.status(500).send(error.message);
+  }
+});
 
 // Setting the port for the server to listen on
 const PORT = process.env.PORT || 3001;
