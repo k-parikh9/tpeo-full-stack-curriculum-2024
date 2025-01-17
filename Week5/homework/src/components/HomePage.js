c010ed21cbda22e12585f4e99477b1fd3b260584
Krish Parikh <kparikh0509@gmail.com>
2025-01-16 22:10:56 -0600

    error text changes

--- Week5/homework/src/components/HomePage.js
+++ Week5/homework/src/components/HomePage.js
@@ -88,7 +88,7 @@ export default function HomePage() {
     })
     .then(response => {
       if (!response.ok) {
-        throw new Error(response.status);
+        throw new Error(response.statusText);
       }
       const updatedTaskList = taskList.filter((t) => t.id !== task.id);
       setTaskList(updatedTaskList);
